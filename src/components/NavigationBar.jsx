a9d72367d6508d78051dff4a3effb4aa9afe0463
NifatHossain <nifathossain1480@gmail.com>
2024-08-15 11:06:36 +0600

    Created navbar

---
+++ src/components/NavigationBar.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Button, Navbar } from "flowbite-react";
+import { NavLink } from "react-router-dom";
+
+const NavigationBar = () => {
+  return (
+    <div>
+      <Navbar fluid rounded>
+        <Navbar.Brand href="/">
+          <img
+            src="./gadget.png"
+            className="mr-3 h-6 sm:h-9"
+            alt="Logo"
+          />
+          <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+            Gadget <span className="text-red-600">Arena</span>
+          </span>
+        </Navbar.Brand>
+        <div className="flex md:order-2">
+          <Button>Log In</Button>
+          <Navbar.Toggle />
+        </div>
+        <Navbar.Collapse>
+          <NavLink to={'/'} active>
+            Home
+          </NavLink>
+          <NavLink to={'/about'}>About</NavLink>
+          <NavLink to={'/'}>Services</NavLink>
+          <NavLink to={'/'}>Pricing</NavLink>
+          <NavLink to={'/contact'}>Contact</NavLink>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
+};
+
+export default NavigationBar;
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import NavigationBar from '../components/NavigationBar';
 
 const Layout = () => {
     return (
         <div className='roboto-font max-w-7xl mx-auto'>
+            <NavigationBar></NavigationBar>
             <Outlet></Outlet>
         </div>
     );
