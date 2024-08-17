8d58a3aef6554fbe3e761e052a143b5b59bbcee3
NifatHossain <nifathossain1480@gmail.com>
2024-08-17 17:41:42 +0600

    Added footer

---
+++ src/components/FooterPart.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+
+import { Footer } from "flowbite-react";
+import {
+  BsDribbble,
+  BsFacebook,
+  BsGithub,
+  BsInstagram,
+  BsTwitter,
+} from "react-icons/bs";
+
+const FooterPart = () => {
+  return (
+    <Footer container className="bg-slate-200">
+      <div className="w-full ">
+        <div className="grid w-full justify-between sm:flex sm:justify-between md:flex md:grid-cols-1">
+          <div className="flex gap-1 items-center">
+            <img src="./gadget.png" className="mr-3 h-6 sm:h-9" alt="Logo" />
+            <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+              Gadget <span className="text-red-600">Arena</span>
+            </span>
+          </div>
+          
+          <div className="grid grid-cols-2 gap-8 sm:mt-4 sm:grid-cols-3 sm:gap-6">
+            <div>
+              <Footer.Title title="about" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="#">Showroom</Footer.Link>
+                <Footer.Link href="#">Events</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Follow us" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="#">Facebook</Footer.Link>
+                <Footer.Link href="#">Youtube</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Legal" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="#">Privacy Policy</Footer.Link>
+                <Footer.Link href="#">Terms &amp; Conditions</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+          </div>
+        </div>
+        <Footer.Divider />
+        <div className="w-full sm:flex sm:items-center sm:justify-between">
+          <Footer.Copyright href="#" by="Gadget-Arena™" year={2024} />
+          <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
+            <Footer.Icon href="#" icon={BsFacebook} />
+            <Footer.Icon href="#" icon={BsInstagram} />
+            <Footer.Icon href="#" icon={BsTwitter} />
+            <Footer.Icon href="#" icon={BsGithub} />
+            <Footer.Icon href="#" icon={BsDribbble} />
+          </div>
+        </div>
+      </div>
+    </Footer>
+  );
+};
+
+export default FooterPart;
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import NavigationBar from '../components/NavigationBar';
+import FooterPart from '../components/FooterPart';
 
 const Layout = () => {
     return (
         <div className='roboto-font max-w-7xl mx-auto'>
             <NavigationBar></NavigationBar>
             <Outlet></Outlet>
+            <FooterPart></FooterPart>
         </div>
     );
 };
